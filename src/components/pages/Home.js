16f256867a2c5b8a834ecc08e13c16a51f2f0310
len07 <83131630+len07@users.noreply.github.com>
2021-12-23 19:30:18 -0500

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../App.css';
-import HeroSection from '../HeroSection';
 import Cards from '../Cards';
+import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 
 function Home () {
