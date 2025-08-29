1bc17b28c5ab27572d3e54f280335db499b34a12
a42luo <alenaluo19@gmail.com>
2025-08-29 17:44:19 -0400

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "",
+  assetPrefix: "",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
